e63efb78607630546b4e62ec996b04bb07c62bd5
Eswar <g.eswar2001@gmail.com>
2026-03-24 19:32:39 +0530

    Update requirements: remove face-recognition and update versions

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 # exit on error
 set -o errexit
 
-export CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+# Install torch-cpu and torchvision-cpu to save ~1GB of storage
+echo "Installing CPU-only PyTorch..."
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+
+# Install the rest of the requirements
 pip install -r requirements.txt
 
+# Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
